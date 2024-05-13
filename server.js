03f9a82c3445513660ef0024910de8372f19b156
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-13 01:13:43 -0300

    fixed the path of the routes

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const port = 3000;
 
 app.get('/', (req, res)=>{
     //res.send('hello express :)');
-    res.sendFile(path.join(__dirname, '.provided/static/index.html'));
+    res.sendFile(path.join(__dirname, './provided/static/index.html'));
 
 });
 
 app.get('/speakers', (req, res)=>{
     //res.send('hello express :)');
-    res.sendFile(path.join(__dirname, '.provided/static/speakers.html'));
+    res.sendFile(path.join(__dirname, './provided/static/speakers.html'));
 
 });
 
