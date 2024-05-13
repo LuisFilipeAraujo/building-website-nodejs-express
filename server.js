3d9f16423571a74aa498cbdbc7257e9673ad1468
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-13 01:08:43 -0300

    added a route for the index.html and speakers.html in the index.js file. This was done using res.sendFile (path.join(__dirname, 'pathname'))

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express =require('express');
+const path =require('path');
 
 const app =express();
 
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('hello express :)');
+    //res.send('hello express :)');
+    res.sendFile(path.join(__dirname, '.static/index.html'));
 
 });
 
+app.get('/speakers', (req, res)=>{
+    //res.send('hello express :)');
+    res.sendFile(path.join(__dirname, '.static/speakers.html'));
+
+});
 
 app.listen(port, ()=>{
     console.log(`Express server listening on port ${port}!`);
