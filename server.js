c2617c4defa19f013776ed8f4cc01e4083647833
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-14 00:45:10 -0300

    sunig middleware static for css configuration

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app =express();
 
 const port = 3000;
 
+//using middleware static
+app.use(express.static(path.join(__dirname, './provided/static')));
+
 app.get('/', (req, res)=>{
     //res.send('hello express :)');
     res.sendFile(path.join(__dirname, './provided/static/index.html'));
