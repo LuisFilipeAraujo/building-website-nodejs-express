5646cd84cabe3a52c88c6f48978a01beb3348715
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-14 20:22:21 -0300

    cloned initial repository

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+const express = require('express');
 
-app.get('/', (req, res)=>{
+//routrer object (I can now replace the app.get for router.get)
+const router = express.Router();
+
+router.get('/', (req, res)=>{
     //res.send('hello express :)');
     res.sendFile(path.join(__dirname, './provided/static/index.html'));
 
